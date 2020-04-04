8b4abe4c7e8ca0dd408e9b063fb65496ed6cb642
nukiz <61660487+nukiz@users.noreply.github.com>
2020-04-04 15:39:50 +0300

    add files

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        private const string Title = "SimpleCalculator v1";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple calculator written in C#. Made by nukiz.");
+            Console.WriteLine("Initializing...")
+
+            ; Console.Title = Title;
+
+            Console.WriteLine("Succesfully Initialized! Starting...");
+
+
+
+
+            Console.Clear();
+
+            Console.Write("Specify an number to calculate: ");
+            double firstNum = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Specify an operator: ");
+            string calculationOp = Console.ReadLine();
+
+            Console.Write("Specify the second number to calculate: ");
+            double secondNum = Convert.ToDouble(Console.ReadLine());
+
+            if (calculationOp == "+")
+            {
+                Console.WriteLine(firstNum + secondNum);
+            } else if (calculationOp == "-") {
+                Console.WriteLine(firstNum - secondNum);
+            }
+            else if (calculationOp == "*")
+            {
+                Console.WriteLine(firstNum * secondNum);
+            }
+            else if (calculationOp == "/")
+            {
+                Console.WriteLine(firstNum / secondNum);
+            }
+            else 
+            {
+                Console.WriteLine("Invalid Operator!");
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+
+
+        
+
+        
+
+        
+    }    
+}
